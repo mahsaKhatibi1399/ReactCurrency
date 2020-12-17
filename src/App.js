edd6786713bb43337ee51d75cc3bec3155669b5c
mahsakhatibi <mahsakhatibi1399@gmail.com>
2020-12-17 23:14:46 +0330

    add axios to project && get data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import CurrencyData from './CurrencyData';
 
 function App() {
+
+  const [value, setValue] = useState();
+
+  const getDataHandler = () => {
+
+    axios.get('https://currency.jafari.li/json')
+
+      .then((resp) => {
+        if (resp.status === 200) {
+
+          setValue(resp.data)
+        }
+      })
+  }
+  console.log('value', value)
+  useEffect(() => {
+    async function getData() {
+      await getDataHandler();
+    }
+    getData();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/CurrencyData.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CurrencyData = () => {
+    return (
+        <div style={{ background: 'red', margin: '10px' }}>
+            1
+        </div>
+    )
+}
+
+export default CurrencyData
