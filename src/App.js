4c37f979a8879db9fbe7dab0f67eebab9b2808aa
mahsakhatibi <mahsakhatibi1399@gmail.com>
2020-12-24 20:05:50 +0330

    change api && set new Api Ui

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import CurrencyData from './CurrencyData';
 
 function App() {
 
-  const [value, setValue] = useState();
+  const [value, setValue] = useState(null);
 
   const getDataHandler = () => {
 
-    axios.get('https://currency.jafari.li/json')
+    axios.get('https://gist.githubusercontent.com/ahmadpanah/e6225b11235a04397b813d0de6e6f5ad/raw/ab559a1146b5e6f2d57bd546c20c090842d5fcc6/sana.json')
 
       .then((resp) => {
         if (resp.status === 200) {
-
-          setValue(resp.data)
+          console.log('res', resp.data.sana.data)
+          setValue(resp.data.sana.data)
         }
       })
+      .catch(err => console.log('errors', err))
   }
-  console.log('value', value)
+
   useEffect(() => {
     async function getData() {
       await getDataHandler();
@@ -28,7 +29,9 @@ function App() {
 
   return (
     <div style={{ backgroundColor: 'currentColor', height: '100%' }}>
-      {value.Currency.map(data => <CurrencyData data={data} />)}
+      <div style={{ padding: '1px' }} >
+        {!!value ? value.map(data => <CurrencyData key={data.slug} data={data} />) : ''}
+      </div>
     </div>
   );
 }
--- src/CurrencyData.jsx
+++ src/CurrencyData.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-const CurrencyData = () => {
+const CurrencyData = ({ data }) => {
     return (
-        <div style={{ background: 'red', margin: '10px' }}>
-            1
+        <div style={{ width: '50%', height: '100px', borderRadius: '5px', background: '#c7c7c7', margin: '10px' }}>
+            <div>
+                <span> {data.title} </span>
+            </div>
+            <div>
+                <span> قیمت : </span>
+                <span>  {data.p} تومان </span>
+
+                <span> حداکثر قیمت : </span>
+                <span>{data.h}</span>
+
+                <span> حداقل قیمت : </span>
+                <span>{data.l}</span>
+            </div>
+            <div>
+                <span> آخرین تغییرات : </span>
+                <span>{data.updated_at}</span>
+            </div>
+
         </div>
     )
 }
 
-export default CurrencyData
+export default CurrencyData;
